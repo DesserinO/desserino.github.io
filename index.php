a25c7badacc5342eacbc1372eca253c3dac1e0ae
DesserinO <forobzor@gmail.com>
2022-04-13 17:19:29 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Личный кабинет</title>
+</head>
+<body>
+	<div class="header">
+		<div class="profile-info">
+			<a class="name" id="name" href="#">
+				<p>Громушкин Максим Валерьевич</p>
+			</a>
+		</div>		
+	</div>
+	<div class="content">
+		<div class="container">
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card 1">
+	  			<div class="card-image"> 
+	  				<img src="file:///C:/Users/mmaks/Desktop/Без%20названия_0_large.jpg" /> 
+	  			</div>
+	  			<div class="card-title title-white">
+	    			<p>Физика</p>
+				</div>
+				<div class="card-info title-white qi-text">
+					<div class="question-quant qi-green">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-yellow">
+						<p>20</p>
+					</div>
+					<div class="question-quant qi-red">
+						<p>20</p>
+					</div>
+				</div>
+				<div class="question-info title-white">
+					<div class="qi-cont">
+						<div class="qi-counter">
+							<p class="qi-quant">127</p>
+						</div>
+						<div class="qi-text-field">
+							<p>Вопросы</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="clearfix"></div>
+		</div>
+	</div>
+
+	
+</body>
+</html>
